7cc9f8f7d6a5d7986577b93f65d553836dd1933e
ANIKET762 <aniketindian8@gmail.com>
2026-06-03 18:43:09 +0530

    fix: primsa raw query

--- src/services/semanticSearchService.ts
+++ src/services/semanticSearchService.ts
@@ -1,23 +1,20 @@
-import {prisma} from "../prisma/prismaClient";
+import { prisma } from "../prisma/prismaClient";
 import { generateEmbedding } from "./embeddingService";
+import { Prisma } from "@prisma/client";
 
-export async function semanticSearch(query: string, limit:number=20){
+export async function semanticSearch(query: string, limit: number = 20) {
     const embedding = await generateEmbedding(query);
     const vectorString = `[${embedding.join(",")}]`;
 
-    const results = await prisma.$queryRawUnsafe(
-        `
+    const results = await prisma.$queryRaw<any[]>`
         SELECT
             *,
-            embedding <=> '${vectorString}'::vector AS distance
+            embedding <=> ${vectorString}::vector AS distance
         FROM "Product"
         WHERE embedding IS NOT NULL
         ORDER BY distance ASC
         LIMIT ${limit}
-        `,
-        vectorString,
-        limit
-    );
-    
+    `;
+
     return results;
 }
